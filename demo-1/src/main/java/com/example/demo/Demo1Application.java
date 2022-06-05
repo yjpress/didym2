fd90a15c25627a18cb396f064fb8beee871eafc5
Jun-Gyu Lee <yjpress@hanmail.net>
2022-06-05 10:06:48 +0900

    xzc

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -8,7 +8,7 @@ public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
-		//testnmmmnnmm
+		//testnmmmnnmm yjpress@han
 	}
 
 }
